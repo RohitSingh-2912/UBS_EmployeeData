302cab2141a8b404c03127fe39e96ec86f216350
RohitSingh-2912 <55476901+RohitSingh-2912@users.noreply.github.com>
2019-10-24 13:14:44 +0530

    Add files via upload

---
+++ UBS.EmployeeData.Abstraction/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UBS.EmployeeData.Abstraction
+{
+    public class Employee
+    {
+        [BsonId]
+        [Required ]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Designation is Required")]
+        public string Designation { get; set; }
+
+        [Required(ErrorMessage = "Gender is Required")]
+        [StringLength(10, MinimumLength = 4)]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Age is Required")]
+        [Range(1, 100, ErrorMessage = "Age must be between 1-100 in years.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "DepartmentName is Required")]
+        public string DepartmentName { get; set; }
+
+        [Required(ErrorMessage = "Salary is Required")]
+        public double Salary { get; set; }
+    }
+}
